109d8f05a669c92c72b831cd10485caf3ce4f8e8
Erlan Aytpaev <aytpaev@gmail.com>
2026-06-05 17:10:30 +0600

    Routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/portfolio', function() {
+    return 'Мое портфолио';
+});
+
+Route::get('/services', function() {
+    return '<ul><li>1</li><li>2</li><li>3</li></ul>';
+});
+
+Route::get('/product/{id}', function(int $id) {
+    return "Товар №{$id}";
+})->whereNumber('id');
+
+Route::get('/blog/{category}/{slug}', function($category, $slug) {
+    return "Товар №{$slug} из категории {$category}";
+});
+
+Route::get('/search/{query?}', function(string $query = "Введите поисковый запрос") {
+  return $query;
+});
+
+Route::get('/dashboard', function() {
+    return "dashboard";
+})->name('dashboard');
+
+Route::prefix('admin')->group(function() {
+    Route::get('/users', function() {
+        return "users";
+    });
+    Route::get('/pc', function() {
+        return "pc";
+    });
+    Route::get('/dzen', function() {
+        return "dzen";
+    });
+});
