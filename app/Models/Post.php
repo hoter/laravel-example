6ae3ca7774f0517afbf0e41995d991a1438605db
Erlan Aytpaev <aytpaev@gmail.com>
2026-06-16 11:50:27 +0600

    Add fillable attr

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,11 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Post extends Model
 {
     public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class);
+    }
+
+    #[Fillable([
+        'title', 'slug', 'content', 'excerpt', 'is_published', 'published_at'
+    ])]
+
+    protected function casts(): array
+    {
+        return [
+            'is_published' => 'boolean',
+            'views' => 'integer',
+            'published_at' => 'datetime',
+        ];
     }
 }
