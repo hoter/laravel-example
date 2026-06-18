2ef75d97e08b4d1a43a0dd8ba9a9c1fa5d9549dc
Erlan Aytpaev <aytpaev@gmail.com>
2026-06-18 20:08:51 +0600

    Update tag

--- database/migrations/2026_06_13_092125_create_tags_table.php
+++ database/migrations/2026_06_13_092125_create_tags_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 200);
             $table->string('slug')->unique();
+            $table->timestamp();
         });
     }
 
