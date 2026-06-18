0a0230f8af3a1ae49a50cdebe45f71925720d5cb
Erlan Aytpaev <aytpaev@gmail.com>
2026-06-18 20:07:55 +0600

    Update tag

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+#[Fillable('name', 'slug')]
 class Tag extends Model
 {
+    use HasFactory;
+    
     public function posts(): BelongsToMany
     {
         return $this->belongsToMany(Post::class);
